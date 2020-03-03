7d3ef875496789e7ed32715aabe9efd0229bf3ad
sergeys83 <57372483+sergeys83@users.noreply.github.com>
2020-03-03 13:50:57 +0300

    6

--- Assets/Scripts/Game/PlayerCamera.cs
+++ Assets/Scripts/Game/PlayerCamera.cs
@@ -6,19 +6,25 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     public Vector3 curPosition;
-    [Range(0,1)]
+    [Range(1,10)]
     public float speed;
     private Quaternion curRotation;
     public Transform player;
     public Vector3 curPlayerPos;
-    public Vector3 delta;
+
+    public Vector3 delta ;
+   /* {
+        get { return delta = transform.position - player.position;}
+        set { }
+    }
+    */
 
     public LayerMask lMask;
     
     // Start is called before the first frame update
     void Start()
     {
-        curPosition = player.InverseTransformPoint(transform.position); //transform.position;
+        curPosition = transform.position;
         curRotation = transform.rotation;
         delta = transform.position - player.transform.position;
         curPlayerPos = player.transform.position;
@@ -26,15 +32,13 @@ public class PlayerCamera : MonoBehaviour
 
     private void Update()
     {
-        //transform.position = curPosition; поворот камеры игрока.
+       //transform.position = curPosition; // поворот камеры игрока.
 
-        //  Vector3 newCampos = player.transform.position+ delta;
-        Vector3 newCampos = player.TransformPoint(curPosition);
-        transform.position = Vector3.Slerp(transform.position, newCampos, 3f*Time.deltaTime);
-       // curPosition = transform.position;
-        Quaternion curRotation = Quaternion.LookRotation(player.position - transform.position);
-      //  transform.RotateAround(player.position,transform.right, curRotation/player.transform.rotation*Time.deltaTime);
-        transform.rotation = Quaternion.Slerp(transform.rotation, curRotation, 3f * Time.deltaTime);
+        Vector3 newCampos = player.transform.position+ delta;
+        transform.position = Vector3.Slerp(transform.position, newCampos, speed*Time.deltaTime);
+        curPosition = transform.position;
+         Quaternion curRotation = Quaternion.LookRotation(player.position- transform.position); 
+        transform.rotation = Quaternion.Slerp(transform.rotation, curRotation, speed * Time.deltaTime);
 
         RaycastHit hit;
         if (Physics.Raycast(player.position, transform.position - player.transform.position, out hit,Vector3.Distance(transform.position,player.position),lMask))
